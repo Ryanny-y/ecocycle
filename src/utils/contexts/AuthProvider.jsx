72bcbf100b28723c5c60e20abd1223ac20921c8b
ryanny <ryannymromero@gmail.com>
2025-04-13 21:34:28 -0800

    Try in prod

--- src/utils/contexts/AuthProvider.jsx
+++ src/utils/contexts/AuthProvider.jsx
@@ -1,4 +1,9 @@
 import { createContext, useEffect, useState } from "react";
+import { jwtDecode } from 'jwt-decode';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc);
 
 export const authContext = createContext();
 
@@ -8,6 +13,17 @@ const AuthProvider = ({ children }) => {
   const [ accessToken, setAccessToken ] = useState('');
   const [ isAuthenticated, setIsAuthenticated ] = useState(false);
   
+  useEffect(() => {
+    if(!accessToken) return;
+
+    const decoded = jwtDecode(accessToken);
+    const exp = decoded.exp * 1000;
+    const expDate = dayjs(exp).utc().format('YYYY/MM/DD HH:MM:ss');
+    console.log(expDate);
+    
+
+  }, [accessToken, userData])
+
   const value = {
     userData, setUserData,
     accessToken, setAccessToken,
